5b29dec13723ad5308f2dc7f0f148de54f1d3aee
Zachary Dodds <dodds@cs.hmc.edu>
2026-05-16 11:03:16 -0700

    updated to use the cgi library, which requires installing legacy-cgi

--- full_stack_local/cgi-bin/reverse_case.py
+++ full_stack_local/cgi-bin/reverse_case.py
@@ -2,17 +2,13 @@
 
 import os
 import sys
-from urllib.parse import parse_qs
-
+import cgi
 
 def reverse_capitalization(text):
     return text.swapcase()
 
-
-content_length = int(os.environ.get("CONTENT_LENGTH", "0"))
-request_body = sys.stdin.read(content_length)
-form = parse_qs(request_body)
-input_text = form.get("text", [""])[0]
+form = cgi.FieldStorage()
+input_text = form.getfirst("text", "")
 result = reverse_capitalization(input_text)
 
 print("Content-Type: text/plain")
