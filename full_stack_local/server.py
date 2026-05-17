56accb548fa2a86417e9839107ac8800a91ad555
Zachary Dodds <dodds@cs.hmc.edu>
2026-05-17 16:50:20 -0700

    updated the server a bit

--- full_stack_local/server.py
+++ full_stack_local/server.py
@@ -1,20 +1,19 @@
-from http.server import CGIHTTPRequestHandler, HTTPServer
-
+from http.server import CGIHTTPRequestHandler, HTTPServer     # crossed-out == deprecated. It's ok for now!
 
 HOST = "localhost"
 PORT = 8000
 
-
 def main():
     server_address = (HOST, PORT)
-    handler = CGIHTTPRequestHandler
-    handler.cgi_directories = ["/cgi-bin"]
+    handler = CGIHTTPRequestHandler          # although deprecated, this mirrors the workings of the knuth server
+    handler.cgi_directories = ["/cgi-bin"]   # location of the Python scripts that handle webpage requests
 
     with HTTPServer(server_address, handler) as server:
         print(f"Serving at http://{HOST}:{PORT}")
         print("Press Ctrl+C to stop.")
         server.serve_forever()
+        # the above line is an "infinite loop" 
+        # it keeps the server running until it's stopped with Ctrl+c (or killing the window) Ctrl+c is better!
 
-
-if __name__ == "__main__":
+if __name__ == "__main__":                   # This test is True when this file is run directly. (False when imported)
     main()
